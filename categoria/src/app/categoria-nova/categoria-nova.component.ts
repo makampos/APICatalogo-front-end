ba0d5f324aa03a298e8de2c0d3af21cce38a74ca
makampos <matheus.campos@edu.unifil.br>
2020-12-24 12:04:00 -0300

    feat: Implementação do componente categoria-nova

--- categoria/src/app/categoria-nova/categoria-nova.component.ts
+++ categoria/src/app/categoria-nova/categoria-nova.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/services/api.service';
 
 @Component({
   selector: 'app-categoria-nova',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria-nova.component.css']
 })
 export class CategoriaNovaComponent implements OnInit {
-
-  constructor() { }
+  categoriaForm: FormGroup;
+  nome: string = ''
+  imagemUrl: String = '';
+  isLoadingResults = false;
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.categoriaForm = this.formBuilder.group ({
+      'nome': [null, Validators.required],
+      'imagemUrl': [null, Validators.required]
+    });
   }
 
+  addCategoria(form: NgForm) {
+    this.isLoadingResults = true;
+    this.api.addCategoria(form)
+    .subscribe(res => {
+      const id = res['id'];
+      this.isLoadingResults = false;
+      this.router.navigate(['/categorias']);
+    }, (err) => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
+  }
 }
