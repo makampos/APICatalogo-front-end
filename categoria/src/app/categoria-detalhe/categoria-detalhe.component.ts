c24b4ac468d0f770c43e79d3bf0ec8b33821175a
makampos <matheus.campos@edu.unifil.br>
2020-12-24 12:46:24 -0300

    feat: Implementação do componente categoria-detalhe

--- categoria/src/app/categoria-detalhe/categoria-detalhe.component.ts
+++ categoria/src/app/categoria-detalhe/categoria-detalhe.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Categoria } from 'src/model/categoria';
+import { ApiService } from 'src/services/api.service';
 
 @Component({
   selector: 'app-categoria-detalhe',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categoria-detalhe.component.css']
 })
 export class CategoriaDetalheComponent implements OnInit {
-
-  constructor() { }
+  categoria: Categoria = { 
+  categoriaId: '',
+  nome: '', 
+  imagemUrl: ''
+}
+  isLoadingResults = true;
+  constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private FormBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.getCategoria(this.route.snapshot.params['id']);
   }
 
+  getCategoria(id){
+    this.api.getCategoria(id)
+      .subscribe(data => {
+        this.categoria = data;
+          console.log(this.categoria);
+            this.isLoadingResults = false;
+    });
+  }
+
+  DeleteCategoria(id) {
+   this.isLoadingResults = true;
+    this.api.deleteCategoria(id)
+      .subscribe(res => {
+        this.isLoadingResults = false;
+          this.router.navigate(['/categorias']);
+      }, (err) => {
+        console.log(err);
+          this.isLoadingResults = false;
+      });
+  }
 }
